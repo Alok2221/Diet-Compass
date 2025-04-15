7c7a8bec09b04e80e9893c109ccbee06cc9428a3
azadubiec <aleksander.zadubiec@gmail.com>
2025-04-15 14:24:40 +0200

    main app run file renamed

--- src/main/java/com/example/dietcompass/DietCompassApp.java
+++ src/main/java/com/example/dietcompass/DietCompassApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DietcompassApplication {
+public class DietCompassApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DietcompassApplication.class, args);
+		SpringApplication.run(DietCompassApp.class, args);
 	}
 
 }
