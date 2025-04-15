0218431a7ba2858fbf6e9d1f1115d9fa10456b60
azadubiec <aleksander.zadubiec@gmail.com>
2025-04-15 14:22:40 +0200

    innit commit

---
+++ src/main/java/com/example/dietcompass/DietcompassApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietcompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietcompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dietcompass/DietcompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietcompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
